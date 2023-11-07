db943a5000df2d51430ecc91d909e809213f9c28
Glaubert Nunes <glaubert@outlook.com>
2023-11-07 06:45:22 -0300

    Adicionado a funcionalidade do teclado

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,9 +4,7 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
 
@@ -18,5 +16,16 @@ while(contador < listaDeTeclas.length) {
         tocaSom(idAudio);
     }
 
-    contador++;
+    tecla.onkeydown = function (evento) {
+
+        if(evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        ≈
+    }
+
+    tecla.onkeyup = function () {{
+        tecla.classList.remove('ativa');
+    }
+
 }
